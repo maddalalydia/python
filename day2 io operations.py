b34b938efc51db89a9331d2501ecc1f41f5939f8
Lydia Maddala <161550402+maddalalydia@users.noreply.github.com>
2026-02-13 10:58:18 +0530

    Add files via upload

---
+++ day2 io operations.py
@@ -0,0 +1,34 @@
+'''#read input numbers from user
+num = input()
+print(type(num)) #checking the type of numb
+print(num)'''
+
+'''#implicit type conversion
+num1 = int(input())
+val = float(input())
+print(type(num1), type(val)) #checking the type of numb
+res = num1+val
+print(res)'''
+
+'''num1 = input()
+print(type(num1)) #checking the type of numb
+res =int(num1) + 10
+print(res)'''
+
+'''num1 = input()
+print(type(num1)) #checking the type of numb
+res =num1 + 10
+print(res)
+#it get error to over come add int in res'''
+
+'''num1 = input()
+print(type(num1)) #checking the type of numb
+res =int(num1) + 10
+print(res)'''
+
+'''#converting int to float, string to bool
+num = 12
+print(float(num))
+print(str(num))
+print(bool(num))'''
+
