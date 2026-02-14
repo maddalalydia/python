0e5f5d94d55819a8b971147dbd084d29b73c3e4c
Lydia Maddala <161550402+maddalalydia@users.noreply.github.com>
2026-02-14 10:38:45 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,23 @@
+'''n='20.56'
+int(n) # if we convert directly we get the value error'''
+
+'''num = "45.89"   # float value stored as string
+print(type(num))   # checking the type of num
+# Convert string → float → int
+num_int = int(float(num))
+print(num_int)
+print(type(num_int))
+res = num_int + 10
+print(res)'''
+
+#10 in [1,2,3,4,5] ->F
+#10 not in [1,2,3,4,5] ->T
+
+
+
+
+
+
+
+
+
