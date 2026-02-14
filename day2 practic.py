7e43bded56a9e0ba3819bccbafc7d565a78a5d6c
Lydia Maddala <161550402+maddalalydia@users.noreply.github.com>
2026-02-14 09:22:03 +0530

    Add files via upload

---
+++ day2 practic.py
@@ -0,0 +1,93 @@
+'''num1 = 10.75   # float value
+
+print(type(num1))   # checking the type of num1
+
+# Float to int
+num_int = int(num1)
+print(num_int)
+print(type(num_int))
+
+# Float to tuple
+num_tuple = (num1,)   # converting float into tuple
+print(num_tuple)
+print(type(num_tuple))
+
+# Float to string
+num_str = str(num1)
+print(num_str)
+print(type(num_str))
+
+# Performing addition (like your example)
+res = num1 + 10
+print(res)'''
+
+
+'''num1 = [10, 20, 30, 40]   # list value
+
+print(type(num1))   # checking the type of num1
+
+# List to tuple
+num_tuple = tuple(num1)
+print(num_tuple)
+print(type(num_tuple))
+
+# List to string
+num_str = str(num1)
+print(num_str)
+print(type(num_str))
+
+# List to set
+num_set = set(num1)
+print(num_set)
+print(type(num_set))
+
+# Example operation (since list + 10 is not possible)
+res = num1 + [50]   # adding another list
+print(res)'''
+
+'''num1 = (10, 20, 30, 40)   # tuple value
+
+print(type(num1))   # checking the type of num1
+
+# Tuple to list
+num_list = list(num1)
+print(num_list)
+print(type(num_list))
+
+# Tuple to string
+num_str = str(num1)
+print(num_str)
+print(type(num_str))
+
+# Tuple to set
+num_set = set(num1)
+print(num_set)
+print(type(num_set))
+
+# Example operation (tuple + 10 is not possible)
+res = num1 + (50,)   # adding one more element to tuple
+print(res)'''
+
+'''num1 = {7, 14, 21, 28}   # set value (random numbers)
+
+print(type(num1))   # checking the type of num1
+
+# Set to list
+num_list = list(num1)
+print(num_list)
+print(type(num_list))
+
+# Set to string
+num_str = str(num1)
+print(num_str)
+print(type(num_str))
+
+# Set to tuple
+num_tuple = tuple(num1)
+print(num_tuple)
+print(type(num_tuple))
+
+# Example operation (set + 10 is not possible)
+# So we convert set to list and then add a number
+res = list(num1) + [10]
+print(res)'''
