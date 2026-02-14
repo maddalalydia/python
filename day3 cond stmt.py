5ca93b55c9c9d6ae9408bf1b9543ad7903797de7
Lydia Maddala <161550402+maddalalydia@users.noreply.github.com>
2026-02-14 11:00:49 +0530

    Add files via upload

---
+++ day3 cond stmt.py
@@ -0,0 +1,32 @@
+'''#to check weather the number is zero or not(using if cond)
+num = int(input())
+if num == 0:
+            print("Number is zero")
+if num != 0:
+    print("not zero")
+print("Program is done")'''
+
+'''#to check weather the number is zero or not(using if - else cond)
+num = int(input())
+if num == 0:
+            print("Number is zero")
+else:
+    print("not zero")
+print("Program is done")'''
+
+'''num = int(input())
+if num != 0:
+            print("Number is not  zero")
+else:
+    print("number is  zero")
+print("Program is done")'''
+
+#to find the bigger numb
+n = int(input())
+m = int(input())
+if(n>>m):
+    print("the large number is :",n )
+else:
+    print("the bigger number is : ",m)
+
+    
