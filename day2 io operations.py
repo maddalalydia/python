e7fa302d955b9457321276721cf903338924816b
Lydia Maddala <161550402+maddalalydia@users.noreply.github.com>
2026-02-14 09:10:51 +0530

    Add files via upload

--- day2 io operations.py
+++ day2 io operations.py
@@ -32,3 +32,26 @@ print(float(num))
 print(str(num))
 print(bool(num))'''
 
+num1 = 10.75   # float value
+
+print(type(num1))   # checking the type of num1
+
+# Float to int
+num_int = int(num1)
+print(num_int)
+print(type(num_int))
+
+# Float to tuple
+num_tuple = (num1,)   # converting float into tuple
+print(num_tuple)
+print(type(num_tuple))
+
+# Float to string
+num_str = str(num1)
+print(num_str)
+print(type(num_str))
+
+# Performing addition (like your example)
+res = num1 + 10
+print(res)
+
