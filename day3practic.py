83bf82971b28123bacc3f4cb6de9f7564662628d
Lydia Maddala <161550402+maddalalydia@users.noreply.github.com>
2026-02-16 09:33:34 +0530

    Add files via upload

---
+++ day3practic.py
@@ -0,0 +1,29 @@
+# to find weather a number is a even or odd
+'''n = int(input())
+if(n%2==0):
+    print("n is even number")
+else:
+    print("n is an odd number")'''
+
+#to check weather the number is divisible by 5 or not
+'''n = int(input())
+if(n%5==0):
+    print("n is divisible by 5")
+else:
+    print("n is not divisible by 5")'''
+
+#to check weather the number is divisible by both the 5 & 3
+'''n = int(input())
+if(n%5==0 & n%3==0):
+    print("n is divisible by both 5 & 3")
+else:
+    print("n is not divisible by both 5 & 3")'''
+
+#to check weather a number is divisible by 4 or 6
+'''n = int(input())
+if(n%4==0):
+    print("n is divisible by 4")
+if(n%6==0):
+    print("n is divisible by 6")
+else:
+    print("the number is not divisible by any of the number")'''
