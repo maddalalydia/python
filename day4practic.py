b26cf798849f1b53316a18a32a28e28903ad29d1
Lydia Maddala <161550402+maddalalydia@users.noreply.github.com>
2026-02-19 09:10:19 +0530

    Add files via upload

---
+++ day4practic.py
@@ -0,0 +1,42 @@
+# checking the gratest num among 3 numbers if they are equal also
+'''n1,n2,n3 = map(int,input().split())
+if(n1>n2):
+    if(n1>n3):
+        print("n1 is gratest number")
+if(n2>n3):
+     print("n2 is gratest number")
+else:
+    print("n3 is grater number")'''
+# checking the gratest num among 3 numbers
+'''n1,n2,n3 = map(int,input().split())
+if(n1>n2):
+    if(n1>n3):
+        print("n1 is gratest number")
+if(n2>n3):
+     print("n2 is gratest number")
+else:
+    print("n3 is grater number")'''
+# if equal values
+'''n1,n2,n3 = map(int,input().split())
+if(n1==n2==n3):
+    print(n1)
+    if(n1==n2 and n2>n3):
+        print(n2)
+else:
+    print(n3)'''
+
+#voting eligible using nested if
+'''age = int(input("Enter your age: "))
+if(age<18):
+    print("not eligible to vote.")
+    exit()
+citizen = input("Are you a citizen? (yes/no): ")
+if age >= 18:
+    if citizen.lower() == "yes":
+        print("You are eligible to vote.")
+    else:
+        print("You are not eligible to vote (Not a citizen).")'''
+
+
+
+
